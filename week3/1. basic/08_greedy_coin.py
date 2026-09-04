567071e23cb8e42f971d4626c8257adc3a3fcff5
1224kang <cherrykang2003@gmail.com>
2026-09-04 22:50:22 +0900

    WEEK3/Basic/08_greedy_coin.py 풀이 완료

--- week3/1. basic/08_greedy_coin.py
+++ week3/1. basic/08_greedy_coin.py
@@ -23,10 +23,7 @@
 10원: 1개
 총 6개
 
-힌트:
-- 큰 동전부터 사용
-- 현재 동전으로 최대한 거슬러주기
-- 나머지 금액으로 다음 동전 사용
+
 """
 
 def make_change_greedy(change, coins):
@@ -42,13 +39,20 @@ def make_change_greedy(change, coins):
     """
     result = {}
     total_coins = 0
+
     
-    # TODO: 각 동전에 대해 반복
-    ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
-    ## 개수가 0보다 크면 결과에 추가
+    while change>0:
+        for coin in coins:
+            if change // coin>0:
+                total_coins+=change//coin
+                result[coin]=change//coin
+                change=change%coin 
+        return total_coins, result
+    
+    
     pass
     
-    return total_coins, result
+    
 
 # 테스트 케이스
 if __name__ == "__main__":
